f76e324be469acb43793c3e17a3898040010f468
EasyCo <eduardobyeasyco@gmail.com>
2024-08-27 12:47:21 -0300

    feat: create entity and mapper (#2)
    * feat: create user entity and user mapper
    
    * feat: update user service with mapper

--- src/main/java/com/socialhub/service/UserService.java
+++ src/main/java/com/socialhub/service/UserService.java
@@ -2,9 +2,9 @@ package com.socialhub.service;
 
 import com.socialhub.dto.UserInputDTO;
 import com.socialhub.dto.UserOutputDTO;
-import com.socialhub.entity.User; // Supondo que você tenha uma classe User como entidade
+import com.socialhub.entity.User;
 import com.socialhub.repository.UserRepository;
-import com.socialhub.mapper.UserMapper; // Supondo que você tenha um mapper para conversão
+import com.socialhub.mapper.UserMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
